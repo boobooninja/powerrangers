7aa9d476e090d219ed8a9f2b93d7b5ad796822c1
Loren Hale <loren.hale@gmail.com>
2014-03-20 16:02:35 -0700

    add run, scream, drink_coffee to Person class

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,21 @@
 class Person
+  def initialize name, caffeine_level
+    @name           = name
+    @caffeine_level = caffeine_level
+  end
+
+  def run
+    puts "Run Run Running!"
+  end
+
+  def scream
+    puts "AAHHHH!"
+  end
+
+  def drink_coffee
+    @caffeine_level += 1
+    puts "Wow! I am hyped!"
+  end
 end
 
 class PowerRanger
